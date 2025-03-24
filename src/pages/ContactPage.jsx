e6da5888a6b8b4b57fa5a7fe91eccf8b8641125d
Kiran Dhoke <dhokekiran98@gmail.com>
2025-03-24 18:56:09 +0530

    Fetch contact page content from API and fallback to static data on error

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,10 +1,28 @@
 import Contact from "../components/contactPage/Contact";
-const ContactPage = ({ ContactUsHeading, ContactUsSubHeading }) => {
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import HomeStaticData from "../Data/HomeStaticData.json";
+const fetchHomepageContent = async (formattedData) => {
+  const { data } = await axios.get(
+    "https://starfish-app-ca2ju.ondigitalocean.app/api/home-page?populate[home_herosection][populate]=BackgroundImage&populate[projects][populate]=*&populate[publications_slider][populate]=*"
+  );
+  return data.data;
+};
+const ContactPage = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["contactpage-content"],
+    queryFn: fetchHomepageContent,
+  });
+
+  // Use API data if available; fallback to static data on error
+  const apiResponse = error ? HomeStaticData.data : data || {};
+  if (isLoading) return <p>Loading...</p>;
   return (
     <>
       <Contact
-        ContactUsHeading={ContactUsHeading}
-        ContactUsSubHeading={ContactUsSubHeading}
+        ContactUsSubHeading={apiResponse.ContactUsSubHeading}
+        ContactUsHeading={apiResponse.ContactUsHeading}
       />
     </>
   );
